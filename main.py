5998a638d773eadbf626c545993a85787f30d1ae
IdrisChraibi <115438872+IdrisChraibi@users.noreply.github.com>
2022-10-10 08:43:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-s = input("")
+s = input("") # Input variable containing the Roman numerals to be translated
         
-roman = { "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000 }
+roman = { "I" : 1,     # Assign a value to each roman integer using a hashmap
+          "V" : 5, 
+          "X" : 10, 
+          "L" : 50, 
+          "C" : 100, 
+          "D" : 500, 
+          "M" : 1000 }      
 
-result = 0
+result = 0 #  The result after converting the Roman integer to whole numbers
         
-for i in range (len(s)):
-    if i + 1 < len(s) & roman[s[i]] < roman[s[i + 1]]:
-        result -= roman[s[i]]
+for i in range (len(s)): # iterate through the string
+    if i + 1 < len(s) & roman[s[i]] < roman[s[i + 1]]: # "i + 1 < len(s)" is needed so the string index doesnt go out of range, "roman[s[i]] < roman[s[i + 1]]" if the value is decreasing 
+        result = result - roman[s[i]]                          # subtract the value from the result
     else:
-        result += roman[s[i]]
+        result = result + roman[s[i]] # add the value to the result
                 
-print(result)
+print(result) # print the result
